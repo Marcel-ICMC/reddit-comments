caf088d3ddc7925a759c2bb464c78495aa7b0b17
Marcel-ICMC <marcelotoboni@gmail.com>
2023-03-19 06:41:32 -0300

    dummy bot

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+        "fmt"
+
+        "github.com/turnage/graw/reddit"
+)
+
+func main() {
+        bot, err := reddit.NewBotFromAgentFile("lurker-bot.agent", 0)
+        if err != nil {
+                fmt.Println("Failed to create bot handle: ", err)
+                return
+        }
+
+        harvest, err := bot.Listing("/r/golang", "")
+        if err != nil {
+                fmt.Println("Failed to fetch /r/golang: ", err)
+                return
+        }
+
+        for _, post := range harvest.Posts[:5] {
+                fmt.Printf("[%s] posted [%s]\n", post.Author, post.Title)
+        }
+}
