efc5a4ca6504290ab5657e75c693244f2a87bc01
Marcel-ICMC <marcelotoboni@gmail.com>
2023-03-20 06:32:24 -0300

    matching specific post

--- main.go
+++ main.go
@@ -1,25 +1,34 @@
 package main
 
 import (
-        "fmt"
+	"fmt"
+	"regexp"
 
-        "github.com/turnage/graw/reddit"
+	"github.com/turnage/graw/reddit"
 )
 
 func main() {
-        bot, err := reddit.NewBotFromAgentFile("lurker-bot.agent", 0)
-        if err != nil {
-                fmt.Println("Failed to create bot handle: ", err)
-                return
-        }
+	bot, err := reddit.NewBotFromAgentFile("lurker-bot.agent", 0)
+	if err != nil {
+		fmt.Println("Failed to create bot handle: ", err)
+		return
+	}
 
-        harvest, err := bot.Listing("/r/golang", "")
-        if err != nil {
-                fmt.Println("Failed to fetch /r/golang: ", err)
-                return
-        }
+	harvest, err := bot.Listing("/r/anime", "")
+	if err != nil {
+		fmt.Println("Failed to fetch /r/anime: ", err)
+		return
+	}
 
-        for _, post := range harvest.Posts[:5] {
-                fmt.Printf("[%s] posted [%s]\n", post.Author, post.Title)
-        }
+	counter := 0
+	for _, post := range harvest.Posts {
+		if match, _ := regexp.MatchString(".+ - Episode \\d+ discussion", post.Title); match {
+			fmt.Println(match)
+			fmt.Printf("[%s] posted [%s] [%s]\n", post.Author, post.Title, post.URL)
+		}
+		counter++
+		if counter == 1000 {
+			break
+		}
+	}
 }
