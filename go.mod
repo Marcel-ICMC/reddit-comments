cab0b81d809af5857e45d5041e12eed51d6ebbfc
Marcel-ICMC <marcelotoboni@gmail.com>
2023-04-02 06:42:31 -0300

    saving thread to a json file

--- go.mod
+++ go.mod
@@ -6,9 +6,11 @@ require github.com/turnage/graw v0.0.0-20201204201853-a177df1b5c91
 
 require (
 	github.com/golang/protobuf v1.3.2 // indirect
+	github.com/google/go-querystring v1.0.0 // indirect
 	github.com/mitchellh/mapstructure v1.1.2 // indirect
 	github.com/turnage/redditproto v0.0.0-20151223012412-afedf1b6eddb // indirect
+	github.com/vartanbeno/go-reddit/v2 v2.0.1 // indirect
 	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553 // indirect
-	golang.org/x/oauth2 v0.0.0-20191202225959-858c2ad4c8b6 // indirect
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
 	google.golang.org/appengine v1.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,25 +1,55 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 	"regexp"
 
 	"github.com/turnage/graw/reddit"
 )
 
-func countComments(replies []*reddit.Comment) int {
-	counter := len(replies)
-	for _, r := range replies {
-		fmt.Println("Replies: ", len(r.Replies), " Body:", r.Body)
-		counter += countComments(r.Replies)
+func getAllComments(bot reddit.Bot, post *reddit.Post) {
+	var queue []*reddit.Comment
+	queue = append(queue, post.Replies...)
+	for len(queue) != 0 {
+		fmt.Println(len(queue))
+		r := queue[0]
+		queue = queue[1:]
+		if r.More != nil {
+			morechildren := map[string]string{
+				"link_id":  "t3_" + post.ID,
+				"children": r.More.Children[0],
+			}
+			harvest, err := bot.ListingWithParams("/api/morechildren.json", morechildren)
+			if err != nil {
+				panic(err)
+			}
+			r.Replies = append(r.Replies, harvest.Comments...)
+		}
+		queue = append(queue, r.Replies...)
 	}
-
-	return counter
 }
 
-func getComments(bot reddit.Bot, permalink string) {
+func writeComments(bot reddit.Bot, permalink string) {
+	fmt.Println(permalink)
 	post, _ := bot.Thread(permalink)
-	fmt.Println("comment count: ", countComments(post.Replies))
+	getAllComments(bot, post)
+	thread_json, _ := json.Marshal(post)
+
+	f, err := os.Create("threads/" + post.ID + ".json")
+	if err != nil {
+		panic(err)
+	}
+
+	defer f.Close()
+
+	nbytes, err := f.Write(thread_json)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("wrote %d bytes\n", nbytes)
+
 }
 
 func main() {
@@ -39,7 +69,7 @@ func main() {
 	for _, post := range harvest.Posts {
 		match := re.FindSubmatch([]byte(post.Title))
 		if len(match) > 0 {
-			getComments(bot, post.Permalink)
+			writeComments(bot, post.Permalink)
 			fmt.Println(string(match[1]), string(match[2]))
 			fmt.Printf("[%s] posted [%s] [%s]\n", post.Author, post.Title, post.URL)
 			break
